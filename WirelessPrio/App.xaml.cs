2de9d2597be78c9b72777fbd8166990bca1a74bd
Kristof Mattei <github@kristofmattei.be>
2012-12-26 10:40:19 +0100

    Initial commit

---
+++ WirelessPrio/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using Microsoft.Shell;
+
+namespace WirelessPrio
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    ///     This page is compiled as Page, not as ApplicationDefinition to omit generation of Main method
+    /// </summary>
+    public partial class App : Application, ISingleInstanceApp
+    {
+        #region ISingleInstanceApp Members
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ WirelessPrio/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Shell;
+
+namespace WirelessPrio
+{
+    public static class EntryPoint
+    {
+        private const string Unique = "01E9936D-271D-4B30-9E38-4D6BEA78E012";
+
+        /// <summary>
+        /// 	Application Entry Point.
+        /// </summary>
+        [STAThread()]
+        public static void Main()
+        {
+            if (!SingleInstance<App>.InitializeAsFirstInstance(Unique)) 
+                return;
+
+            var application = new App();
+
+            application.InitializeComponent();
+            application.Run();
+
+            // Allow single instance code to perform cleanup operations
+            SingleInstance<App>.Cleanup();
+        }
+    }
+}
