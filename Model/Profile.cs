cc84f21a1c2d60b110577225b16efbcd7892fc7f
Kristof Mattei <github@kristofmattei.be>
2012-12-30 13:18:46 +0100

    Modified constructor

--- Model/Profile.cs
+++ Model/Profile.cs
@@ -1,10 +1,17 @@
 ﻿namespace Model
 {
 	/// <summary>
-	/// Defines a wireless profile (ssid, ...)
+	///     Defines a wireless profile (index and name)
 	/// </summary>
 	public class Profile
 	{
-		 
+		public Profile(int index, string profileName)
+		{
+			this.Index = index;
+			this.ProfileName = profileName;
+		}
+
+		public string ProfileName { get; private set; }
+		public int Index { get; private set; }
 	}
 }
